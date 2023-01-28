741056c510cb48d08147998ee33d2ad08e2a4693
mossbee <69780142+mossbee@users.noreply.github.com>
2023-01-28 23:58:46 +0700

    Add files via upload

---
+++ Painful/Painful/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Painful
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEraser = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnPencil = new System.Windows.Forms.Button();
+            this.picDrawingSurface = new System.Windows.Forms.PictureBox();
+            this.pnlButton = new System.Windows.Forms.Panel();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picDrawingSurface)).BeginInit();
+            this.pnlButton.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEraser
+            // 
+            this.btnEraser.Location = new System.Drawing.Point(240, 12);
+            this.btnEraser.Name = "btnEraser";
+            this.btnEraser.Size = new System.Drawing.Size(108, 42);
+            this.btnEraser.TabIndex = 2;
+            this.btnEraser.Text = "Eraser";
+            this.btnEraser.UseVisualStyleBackColor = true;
+            this.btnEraser.Click += new System.EventHandler(this.btnEraser_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(126, 12);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(108, 42);
+            this.btnRectangle.TabIndex = 1;
+            this.btnRectangle.Text = "Rect";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnPencil
+            // 
+            this.btnPencil.Location = new System.Drawing.Point(12, 12);
+            this.btnPencil.Name = "btnPencil";
+            this.btnPencil.Size = new System.Drawing.Size(108, 42);
+            this.btnPencil.TabIndex = 0;
+            this.btnPencil.Text = "Pencil";
+            this.btnPencil.UseVisualStyleBackColor = true;
+            this.btnPencil.Click += new System.EventHandler(this.btnPencil_Click);
+            // 
+            // picDrawingSurface
+            // 
+            this.picDrawingSurface.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picDrawingSurface.BackColor = System.Drawing.Color.White;
+            this.picDrawingSurface.Location = new System.Drawing.Point(0, 0);
+            this.picDrawingSurface.Name = "picDrawingSurface";
+            this.picDrawingSurface.Size = new System.Drawing.Size(1178, 744);
+            this.picDrawingSurface.TabIndex = 4;
+            this.picDrawingSurface.TabStop = false;
+            this.picDrawingSurface.Paint += new System.Windows.Forms.PaintEventHandler(this.picDrawingSurface_Paint);
+            this.picDrawingSurface.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picDrawingSurface_MouseDown);
+            this.picDrawingSurface.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDrawingSurface_MouseMove);
+            this.picDrawingSurface.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picDrawingSurface_MouseUp);
+            // 
+            // pnlButton
+            // 
+            this.pnlButton.Controls.Add(this.btnColor);
+            this.pnlButton.Controls.Add(this.btnPencil);
+            this.pnlButton.Controls.Add(this.btnRectangle);
+            this.pnlButton.Controls.Add(this.btnEraser);
+            this.pnlButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlButton.Location = new System.Drawing.Point(0, 0);
+            this.pnlButton.Name = "pnlButton";
+            this.pnlButton.Size = new System.Drawing.Size(1178, 78);
+            this.pnlButton.TabIndex = 5;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(354, 13);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(108, 41);
+            this.btnColor.TabIndex = 3;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 744);
+            this.Controls.Add(this.pnlButton);
+            this.Controls.Add(this.picDrawingSurface);
+            this.MaximumSize = new System.Drawing.Size(1200, 800);
+            this.MinimumSize = new System.Drawing.Size(1200, 800);
+            this.Name = "Form1";
+            this.Text = "Pain";
+            ((System.ComponentModel.ISupportInitialize)(this.picDrawingSurface)).EndInit();
+            this.pnlButton.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEraser;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnPencil;
+        private System.Windows.Forms.PictureBox picDrawingSurface;
+        private System.Windows.Forms.Panel pnlButton;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Painful/Painful/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Painful
+{
+    public partial class Form1 : Form
+    {
+        private IDrawingTool currentTool;
+        private List<IDrawingTool> tools;
+        private Bitmap buffer;
+        private Graphics gBuffer;
+        public Color colour;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 800;
+            this.Height = 800;
+
+            buffer = new Bitmap(picDrawingSurface.Width, picDrawingSurface.Height);
+            gBuffer = Graphics.FromImage(buffer);
+
+       
+
+            tools = new List<IDrawingTool>();
+
+            colour = Color.Black;
+
+            SetCurrentTool(new Pencil(colour, 2));
+        }
+
+        private void SetCurrentTool(IDrawingTool tool)
+        {
+            currentTool = tool;
+        }
+
+        private void picDrawingSurface_MouseDown(object sender, MouseEventArgs e)
+        {
+            currentTool.OnMouseDown(e.Location);
+        }
+
+        private void picDrawingSurface_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                currentTool.OnMouseMove(e.Location);
+                currentTool.Draw(gBuffer);
+                picDrawingSurface.Invalidate();
+            }
+        }
+
+        private void picDrawingSurface_MouseUp(object sender, MouseEventArgs e)
+        {
+            currentTool.OnMouseUp(e.Location);
+            tools.Add(currentTool);
+            currentTool = currentTool.Clone();
+        }
+
+        private void picDrawingSurface_Paint(object sender, PaintEventArgs e)
+        {
+            gBuffer.Clear(Color.White);
+            foreach (IDrawingTool tool in tools)
+            {
+                tool.Draw(gBuffer);
+            }
+            currentTool.Draw(gBuffer);
+            e.Graphics.DrawImageUnscaled(buffer, Point.Empty);
+        }
+
+        private void btnPencil_Click(object sender, EventArgs e)
+        {
+            SetCurrentTool(new Pencil(colour, 2));
+        }
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            SetCurrentTool(new Rectangle(colour, 2));
+        }
+
+        private void btnEraser_Click(object sender, EventArgs e)
+        {
+            SetCurrentTool(new Eraser(30));
+        }
+
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                colour = colorDialog1.Color;
+            }
+            SetCurrentTool(new Pencil(colour, 2));
+        }
+    }
+    public interface IDrawingTool
+    {
+        IDrawingTool Clone();
+        void OnMouseDown(Point location);
+        void OnMouseMove(Point location);
+        void OnMouseUp(Point location);
+        void Draw(Graphics g);
+    }
+    public class Pencil : IDrawingTool
+    {
+        private List<Point> points;
+        private Pen pen;
+
+        public Pencil(Color color, int width)
+        {
+            points = new List<Point>();
+            pen = new Pen(color, width);
+        }
+        /// <summary>
+        /// Lưu quá trình vẽ bằng danh sách điểm mỗi khi mousemove và mousedown
+        /// Để vẽ lại vào gBuffer 
+        /// </summary>
+        /// <returns></returns>
+        public IDrawingTool Clone()
+        {
+            Pencil pencil = new Pencil(pen.Color, (int)pen.Width);
+            pencil.points = new List<Point>(points);
+            return pencil;
+        }
+
+        public void OnMouseDown(Point location)
+        {
+            points.Clear();
+            points.Add(location);
+        }
+
+        public void OnMouseMove(Point location)
+        {
+            points.Add(location);
+        }
+
+        public void OnMouseUp(Point location)
+        {
+            points.Add(location);
+        }
+
+        public void Draw(Graphics g)
+        {
+            for (int i = 1; i < points.Count; i++)
+            {
+                g.DrawLine(pen, points[i - 1], points[i]);
+            }
+        }
+    }
+    public class Rectangle : IDrawingTool
+    {
+        private Point startLocation;
+        private Point endLocation;
+        private Pen pen;
+        private Brush brush;
+
+        public Rectangle(Color colour, int width)
+        {
+            pen = new Pen(colour, width);
+            brush = new SolidBrush(colour);
+        }
+
+        public IDrawingTool Clone()
+        {
+            return new Rectangle(pen.Color, (int)pen.Width);
+        }
+        public void OnMouseDown(Point location)
+        {
+            startLocation = location;
+            endLocation = location;
+        }
+
+        public void OnMouseMove(Point location)
+        {
+            endLocation = location;
+        }
+
+        public void OnMouseUp(Point location)
+        {
+            endLocation = location;
+        }
+
+        public void Draw(Graphics g)
+        {
+            int x = Math.Min(startLocation.X, endLocation.X);
+            int y = Math.Min(startLocation.Y, endLocation.Y);
+            int width = Math.Abs(startLocation.X - endLocation.X);
+            int height = Math.Abs(startLocation.Y - endLocation.Y);
+            g.DrawRectangle(pen, x, y, width, height);
+        }
+    }
+    public class Eraser : Pencil
+    {
+        public Eraser(int width) : base(Color.White, width)
+        {
+        }
+    }
+}
